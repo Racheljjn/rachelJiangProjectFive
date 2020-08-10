38507b923ddc75f07737f1cda17cb0148c613705
Racheljjn <jian1160@mylaurier.ca>
2020-08-10 16:41:48 -0400

    Updates

---
+++ precache-manifest.c7406192db18678bd9d602d8834bc804.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba581c2fc12c053010e10a3d35f89d7",
+    "url": "/rachelJiangProjectFive/index.html"
+  },
+  {
+    "revision": "5c1f4115836ffbbd5363",
+    "url": "/rachelJiangProjectFive/static/css/main.190149b2.chunk.css"
+  },
+  {
+    "revision": "6a0ffc72c7095e86e3c9",
+    "url": "/rachelJiangProjectFive/static/js/2.7183b281.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rachelJiangProjectFive/static/js/2.7183b281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c1f4115836ffbbd5363",
+    "url": "/rachelJiangProjectFive/static/js/main.bfc1f1ce.chunk.js"
+  },
+  {
+    "revision": "d76d4edade905b0e93ae",
+    "url": "/rachelJiangProjectFive/static/js/runtime-main.b5b3a147.js"
+  }
+]);
